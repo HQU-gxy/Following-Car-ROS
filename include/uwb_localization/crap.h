ad35db8baade7d09a38509f4da1a5fa5c00fa3e5
crosstyan <crosstyan@outlook.com>
2024-09-28 19:32:32 +0800

    format and third-party libraries

--- include/uwb_localization/crap.h
+++ include/uwb_localization/crap.h
@@ -7,30 +7,28 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "serial/serial.h"
 
-struct uwbData
-{
-    uint16_t distance;
-    float degree;
-    bool paused;
+struct uwbData {
+	uint16_t distance;
+	float degree;
+	bool paused;
 };
 
-class uwb : public rclcpp::Node
-{
+class uwb : public rclcpp::Node {
 public:
-    uwb();
-    void cleanup();
+	uwb();
+	void cleanup();
 
 private:
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-    std::shared_ptr<serial::Serial> uwbSerial;
+	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+	std::shared_ptr<serial::Serial> uwbSerial;
 
-    std::jthread uwbDataThread;
-    std::atomic_bool running;
-    std::string uwbDataStr;
-    bool uwbDataAvail;
+	std::jthread uwbDataThread;
+	std::atomic_bool running;
+	std::string uwbDataStr;
+	bool uwbDataAvail;
 
-    void timerCallback();
-    void uwbDataThreadCb();
-    bool parseData(uwbData &data);
+	void timerCallback();
+	void uwbDataThreadCb();
+	bool parseData(uwbData &data);
 };
